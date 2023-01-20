a522bd0699378a86967ab2536a463246fd27d48f
dimon777ddd <gxgamer727@gmail.com>
2023-01-20 23:53:15 +0400

    lesson 3

---
+++ task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+while (true)
+{
+    Console.Write("Enter a number (or 'stop' to end): ");
+    string input = Console.ReadLine();
+    if (input == "stop")
+    {
+        break;
+    }
+    int number = int.Parse(input);
+    if (number > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("You entered " + count + " numbers greater than 0.");
---
+++ task 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double k1, b1, k2, b2;
+
+        Console.Write("Enter value of k1: ");
+        k1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter value of b1: ");
+        b1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter value of k2: ");
+        k2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter value of b2: ");
+        b2 = double.Parse(Console.ReadLine());
+
+        if (k1 == k2)
+        {
+            Console.WriteLine("The lines are parallel, they do not intersect.");
+        }
+        else
+        {
+            double x = (b2 - b1) / (k1 - k2);
+            double y = k1 * x + b1;
+
+            Console.WriteLine("The point of intersection is: (" + x + ", " + y + ")");
+        }
+
+    }
+}
