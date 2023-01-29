01ea1eaaaf50025ba3a795580bc1baaaecf5cf88
dimon777ddd <gxgamer727@gmail.com>
2023-01-29 23:47:36 +0400

    7 seminar

---
+++ 7 seminar/task 47/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+double[,] array = new double[m, n];
+
+void CreateArrayDouble(double[,] array)
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void WriteArray (double[,] array){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        double alignNumber = Math.Round(array[i, j], 1);
+        Console.Write(alignNumber + " ");
+      }
+      Console.WriteLine();
+  }
+}
+
+CreateArrayDouble(array);
+
+WriteArray(array);
+
+Console.WriteLine();
---
+++ 7 seminar/task 50/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter array size");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = new Random().Next(0,21);  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + " ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Enter coordinates");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("There is no such number");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine();
+ Console.WriteLine(c);
+ }
