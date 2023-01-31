fe37f81edfa7111a980cd5fc650ad0b20a2fa444
dimon777ddd <gxgamer727@gmail.com>
2023-02-01 00:18:08 +0400

    8 seminar

---
+++ 8 seminar/task 58/Program.cs
@@ -0,0 +1,66 @@
+﻿int m = InputNumbers("Enter the number of rows of the 1st matrix: ");
+int n = InputNumbers("Enter the number of columns of the 1st matrix: ");
+int p = InputNumbers("Enter the number of columns of the 2nd matrix: ");
+int range = InputNumbers("Enter a range of random numbers: from 1 to ");
+
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nFirstMartrix:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nSecomdMartrix:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nProduct of the first and second matrices:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
---
+++ 8 seminar/task 62/Program.cs
@@ -0,0 +1,37 @@
+﻿int n = 4;
+int[,] squarematrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= squarematrix.GetLength(0) * squarematrix.GetLength(1))
+{
+  squarematrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < squarematrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= squarematrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > squarematrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(squarematrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
